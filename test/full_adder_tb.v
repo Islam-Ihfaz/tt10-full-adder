b25b15e907481b2efde5be025069c1a9d7e99471
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-06-22 00:26:19 -0400

    Update and rename tb.v to full_adder_tb.v

--- test/full_adder_tb.v
+++ test/full_adder_tb.v
@@ -4,12 +4,12 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module full_adder_tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
+     $dumpfile("full_adder_tb.vcd");
+     $dumpvars(0, full_adder_tb);
     #1;
   end
 
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_islam_ihfaz_full_adder user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
